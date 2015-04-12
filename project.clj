56559f3f9d1a3d74514afc96b0929bb52fb7689e
Carlos Cunha <ccfontes@gmail.com>
2015-04-12 02:27:02 +0100

    Enable option send as :html instead of :body
    Bump version.
    Refactor code.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
-(defproject mailsling "0.1.0"
+(defproject mailsling "0.1.1"
 
   :description "Clojure bindings for http://mailgun.com (API v2)"
 
   :url "https://github.com/ccfontes/mailsling"
 
-  :license {:name "MIT"
-            :url "http://opensource.org/licenses/MIT"}
+  :scm {:name "git", :url "https://github.com/ccfontes/mailsling"}
+
+  :license {:name "MIT", :url "http://opensource.org/licenses/MIT"}
 
   :warn-on-reflection false
 
   :repl-options {:init-ns mail.sling}
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.6.5"]
                  [cheshire "5.0.2"]])
--- src/mail/sling.clj
+++ src/mail/sling.clj
@@ -3,20 +3,15 @@
             [clojure.string :refer (join)]
             [cheshire.core :refer (generate-string parse-string)]))
 
+(defn xor [a b]
+  "True if either a or b is True, but not if both are True or False"
+  (cond
+   (and (= a true) (= b false)) true
+   (and (= a false) (= b true)) true
+   :else false))
+
 (defonce ^:dynamic *api-key* (System/getenv "MAILGUN_API_KEY"))
 (defonce ^:dynamic *domain* (System/getenv "MAILGUN_APP_DOMAIN"))
-
-(declare comma-list)
-(declare to-mailgun)
-
-(defn send-plaintext
-  [api-key domain from {:keys [to subject body]}]
-  {:pre [(string? subject) (string? body)
-         (or (list? to) (string? to))]}
-  (to-mailgun api-key domain {:to (comma-list to)
-                              :from from
-                              :subject subject
-                              :text body}))
 
 (defn- comma-list
   [list]
@@ -26,20 +21,29 @@
 
 (defn- to-mailgun
   "Send an e-mail"
-  [api-key domain form-data]
-  (let [resp (http/post (join ["https://api.mailgun.net/v2/" domain "/messages"])
-                        {:basic-auth ["api" api-key]
+  [form-data]
+  (let [resp (http/post (join ["https://api.mailgun.net/v2/" *domain* "/messages"])
+                        {:basic-auth ["api" *api-key*]
                          :form-params form-data})
         status (:status resp)
         body (parse-string (:body resp))]
     (if (= status 200)
       {:sent 'yes, :id (body "id")})))
 
+(defn shootmail
+  [{:keys [from to subject body html] :as params}]
+  {:pre [(string? subject)
+         (xor (string? body) (string? html))
+         (or (list? to) (string? to))]}
+  (to-mailgun
+    (merge
+      {:from from, :to (comma-list to), :subject subject}
+      (if body {:text body} {:html html}))))
+
 (defmacro with-sling [credentials & body]
   `(binding [*api-key* (or (:api-key ~credentials) *api-key*)
              *domain* (or (:domain ~credentials) *domain*)]
     ~@body))
 
-(defn shootmail [{:keys [from] :as params}]
-  (send-plaintext *api-key* *domain* from
-    (dissoc params :from)))
+; TODO change :body to :text
+; TODO make subject and body optional
